f929c5dcd4473356664b863d8e11dda42e807c1c
Zimian Xu <zimian.xu@thoughtworks.com>
2020-05-09 21:04:11 +0800

    testing run

--- src/main/java/cn/xpbootcamp/locker_robot/Locker.java
+++ src/main/java/cn/xpbootcamp/locker_robot/Locker.java
@@ -1,9 +1,22 @@
 package cn.xpbootcamp.locker_robot;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Locker {
-    public Locker(int rooms) {
+
+    private int roomsCount;
+    private Set<Ticket> rooms = new HashSet<>();
+
+    public Locker(int roomsCount) {
+        this.roomsCount = roomsCount;
     }
 
     public void open() {
+        if (rooms.size() + 1 <= roomsCount) {
+            rooms.add(new Ticket());
+        } else {
+            throw new RuntimeException("");
+        }
     }
 }
