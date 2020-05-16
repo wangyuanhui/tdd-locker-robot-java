e86ed489863f30f9096f0a980445b95b6455d7ae
Zimian Xu <zimian.xu@thoughtworks.com>
2020-05-16 10:41:56 +0800

    add robot put() implement

--- src/main/java/cn/xpbootcamp/locker_robot/domain/Robot.java
+++ src/main/java/cn/xpbootcamp/locker_robot/domain/Robot.java
@@ -1,9 +1,23 @@
 package cn.xpbootcamp.locker_robot.domain;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Robot {
-    public Robot(Locker ...locker) {
+    private List<Locker> lockers;
+
+    public Robot(Locker... lockers) {
+        this.lockers = Arrays.asList(lockers);
     }
 
     public void put(Bag bag) {
+        for (Locker locker : lockers) {
+            if (locker.getStatus() > 0) {
+                locker.put(bag);
+                return;
+            }
+        }
+
+        throw new RuntimeException("All lockers are full");
     }
 }
