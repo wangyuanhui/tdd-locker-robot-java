6e1f5d558faf4f0fd1028578daf27ba467695b16
wang yuanhui <yuanhui.wang@thoughtworks.com>
2020-05-29 21:14:27 +0800

    copy from smart robot

---
+++ src/main/java/cn/xpbootcamp/locker_robot/domain/SuperRobot.java
@@ -0,0 +1,52 @@
+package cn.xpbootcamp.locker_robot.domain;
+
+import cn.xpbootcamp.locker_robot.exception.LockersAreFullException;
+import cn.xpbootcamp.locker_robot.exception.TicketIsInvalidException;
+import cn.xpbootcamp.locker_robot.exception.TicketIsInvalidForRobotException;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SuperRobot {
+
+    private List<Locker> lockers;
+
+    public SuperRobot(Locker... lockers) {
+        this.lockers = Arrays.asList(lockers);
+    }
+
+    public Ticket put(Bag bag) {
+//        int max = 0;
+//        Locker putLocker = null;
+//        for (Locker locker : lockers) {
+//            if (locker.getFreeNum() > max) {
+//                max = locker.getFreeNum();
+//                putLocker = locker;
+//            }
+//        }
+//
+//        if (putLocker == null) {
+//            throw new LockersAreFullException();
+//        }
+//
+//        return putLocker.put(bag);
+        return null;
+    }
+
+    public Bag take(Ticket ticket) {
+//        Bag bag = null;
+//        boolean success = false;
+//        for (Locker locker : lockers) {
+//            try {
+//                bag = locker.take(ticket);
+//                success = true;
+//            } catch (TicketIsInvalidException ignore) {
+//            }
+//        }
+//        if (!success) {
+//            throw new TicketIsInvalidForRobotException();
+//        }
+//        return bag;
+        return null;
+    }
+}
---
+++ src/test/java/cn/xpbootcamp/locker_robot/domain/SuperRobotTest.java
@@ -0,0 +1,92 @@
+package cn.xpbootcamp.locker_robot.domain;
+
+import cn.xpbootcamp.locker_robot.exception.LockersAreFullException;
+import cn.xpbootcamp.locker_robot.exception.TicketIsInvalidForRobotException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SuperRobotTest {
+
+    @Test
+    void should_thrown_exception_when_put_given_all_lockers_full() {
+        Locker locker1 = new Locker(0);
+        Locker locker2 = new Locker(0);
+        SuperRobot superRobot = new SuperRobot(locker1, locker2);
+        Bag bag = new Bag();
+
+        assertThrows(LockersAreFullException.class, () -> superRobot.put(bag));
+    }
+
+    @Test
+    void should_return_ticket_of_locker2_when_put_given_locker_1_has_1_free_in_3_locker_2_has_2_free_in_3() {
+//        Locker locker1 = new Locker(1);
+//        Locker locker2 = new Locker(2);
+//        SuperRobot superRobot = new SuperRobot(locker1, locker2);
+//        Bag bag = new Bag();
+//
+//        Ticket ticket = superRobot.put(bag);
+//
+//        assertNotNull(ticket);
+//        assertEquals(1, locker1.getFreeNum());
+//        assertEquals(1, locker2.getFreeNum());
+//        assertEquals(bag, locker2.take(ticket));
+    }
+
+    @Test
+    void should_return_ticket_of_locker1_when_put_given_locker2_has_1_free_in_2_locker2_has_1_free_in_3() {
+//        Locker locker1 = new Locker(2);
+//        Locker locker2 = new Locker(1);
+//        SuperRobot superRobot = new SuperRobot(locker1, locker2);
+//        Bag bag = new Bag();
+//
+//        Ticket ticket = superRobot.put(bag);
+//
+//        assertNotNull(ticket);
+//        assertEquals(1, locker1.getFreeNum());
+//        assertEquals(1, locker2.getFreeNum());
+//        assertEquals(bag, locker1.take(ticket));
+    }
+
+    @Test
+    void should_return_ticket_of_locker1_or_locker2_when_put_given_locker1_and_locker2_have_same_free_rate() {
+//        Locker locker1 = new Locker(2);
+//        Locker locker2 = new Locker(2);
+//        Locker locker3 = new Locker(1);
+//        SuperRobot superRobot = new SuperRobot(locker1, locker2);
+//        Bag bag = new Bag();
+//        Ticket ticket = superRobot.put(bag);
+//
+//        assertNotNull(ticket);
+//        int free1 = locker1.getFreeNum();
+//        int free2 = locker2.getFreeNum();
+//        int free3 = locker3.getFreeNum();
+//        assertEquals(1, free3);
+//        assertTrue((free1 == 2 && free2 == 1) || (free1 == 1 && free2 == 2));
+    }
+
+    @Test
+    void should_throw_exception_when_take_given_ticket_invalid() {
+        Locker locker1 = new Locker(0);
+        Locker locker2 = new Locker(2);
+        SuperRobot superRobot = new SuperRobot(locker1, locker2);
+        Ticket ticket = new Ticket();
+
+        assertThrows(TicketIsInvalidForRobotException.class, () -> superRobot.take(ticket));
+    }
+
+    @Test
+    void should_return_bag_exception_when_take_given_ticket_valid() {
+        Locker locker1 = new Locker(1);
+        Locker locker2 = new Locker(2);
+        SuperRobot superRobot = new SuperRobot(locker1, locker2);
+        Bag bag = new Bag();
+
+        Ticket ticket = superRobot.put(bag);
+
+        assertEquals(bag, superRobot.take(ticket));
+    }
+}
